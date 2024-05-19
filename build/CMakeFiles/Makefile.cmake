f556c0d3f33d7e45d64d16f1033741fde78a3b5f
pupok <pupok@Ubuntu22.04.4.myguest.virtualbox.org>
2024-05-19 20:36:47 +0200

    Added CMakeLists.txt and built the project

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -43,5 +43,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/BridgeExample.dir/DependInfo.cmake"
+  "CMakeFiles/Bridge.dir/DependInfo.cmake"
   )
