f2bb1f5d36b5d9c08652100343e7da4aff358024
pupok <pupok@Ubuntu22.04.4.myguest.virtualbox.org>
2024-05-19 14:28:42 +0200

    Added CMakeLists.txt and built the project

---
+++ build/CMakeFiles/BridgeExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BridgeExample"
+  "BridgeExample.pdb"
+  "CMakeFiles/BridgeExample.dir/bridge.cpp.o"
+  "CMakeFiles/BridgeExample.dir/bridge.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BridgeExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
